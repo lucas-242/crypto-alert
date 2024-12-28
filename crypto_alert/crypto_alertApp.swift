ef5e3a35bdb79b3155977baddccf2e3a74f50341
Lucas Guimarães <guimaraeslucas242@gmail.com>
2024-12-28 10:36:55 -0300

    Initial Commit

---
+++ crypto_alert/crypto_alertApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_alertApp.swift
+//  crypto_alert
+//
+//  Created by Lucas Matheus Guimarães on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct crypto_alertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ crypto_alertTests/crypto_alertTests.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_alertTests.swift
+//  crypto_alertTests
+//
+//  Created by Lucas Matheus Guimarães on 28/12/24.
+//
+
+import Testing
+@testable import crypto_alert
+
+struct crypto_alertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
